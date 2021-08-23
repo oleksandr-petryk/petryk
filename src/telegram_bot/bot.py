29240d792bd80e9a0967a954734d72bdabda3687
Petryk <57829569+Sasha-hk@users.noreply.github.com>
2021-08-22 22:44:46 -0500

    Some changes

--- src/telegram_bot/bot.py
+++ src/telegram_bot/bot.py
@@ -16,34 +16,45 @@ markup = types.ReplyKeyboardMarkup(row_width=1)
 list_of_messages = types.KeyboardButton('/all_messages')
 last_message = types.KeyboardButton('/last_message')
 markup.row(list_of_messages, last_message)
+ 
 
 
 def update_users_list(to_write): 
     with open(USERS, 'w') as file: 
         json.dump(to_write, file, indent=4)
 
-def user_is_unique(message):
+def check_user(message):
     with open(USERS, 'r') as f:
-        users = json.load(f)
+            users = json.load(f)
 
-        user_not_exist = True
-        for user in users: 
-            if user['user']['id'] == message.from_user.id:
-                user_not_exist = False
-                break
+            user_not_exist = True
+            for user in users: 
+                if user['user']['id'] == message.from_user.id:
+                    user_not_exist = False
+                    break
 
 
-        if user_not_exist: 
-            new_user = {'user': {}} 
+            if user_not_exist: 
+                new_user = {'user': {}} 
 
-            new_user['user']['id'] = message.from_user.id
-            new_user['user']['first_name'] = message.from_user.first_name
-            new_user['user']['username'] = message.from_user.username
-            new_user['user']['username'] = message.from_user.username
-            new_user['date'] = message.date
+                new_user['user']['id'] = message.from_user.id
+                new_user['user']['first_name'] = message.from_user.first_name
+                new_user['user']['username'] = message.from_user.username
+                new_user['user']['username'] = message.from_user.username
+                new_user['date'] = message.date
 
-            users.append(new_user)  
-            update_users_list(users)
+                users.append(new_user)  
+                update_users_list(users)
+
+def user_is_unique(message):
+    if os.path.isfile(USERS):
+        check_user(message)
+
+    else:
+        with open(USERS, 'w') as f:
+            f.write('[]')
+        
+        check_user(message)
 
 
 @bot.message_handler(commands=['start', 'help'])
