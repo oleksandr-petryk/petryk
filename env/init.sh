a42c904c3607ea62a429f499d069c53d2da6a397
Petryk <57829569+Sasha-hk@users.noreply.github.com>
2021-08-23 01:14:23 -0500

    Fix init.sh

--- env/init.sh
+++ env/init.sh
@@ -12,8 +12,7 @@ domain_name=""
 project_name=""
 user_to_run_gunicorn=""
 gunicorn_workers_count=3 
-base_dir="$(dirname "$0")"  
-base_dir="$(dirname "$base_dir")"  
+base_dir="$(dirname "$PWD")"   
 
 
 
