4836537a04b250abb70c3d19be3e3a0bcd096ee5
root <root@ydu.l.hostens.cloud>
2021-09-17 23:09:26 +0300

    Add allowed hosts

--- src/config/settings.py
+++ src/config/settings.py
@@ -9,7 +9,7 @@ DEBUG = True
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['petryk.me', 'www.petryk.me'] 
 
 INSTALLED_APPS = [
     'main',
@@ -57,19 +57,19 @@ TEMPLATES = [
 ]
 
 DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': os.environ.get('DATABASE_NAME'),
+        #'USER': os.environ.get('DATABASE_USER'),
+        #'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        #'HOST': os.environ.get('DATABASE_HOST'),
+        #'PORT': os.environ.get('DATABASE_PORT'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
-    }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     # 'NAME': BASE_DIR / 'db.sqlite3'
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
+         'ENGINE': 'django.db.backends.sqlite3',
+         # 'NAME': BASE_DIR / 'db.sqlite3'
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 }  
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'  
