a9f0c406d9260ed20eae321c0b5e35688311d0f9
Sasha-hk <sachapos50@gmail.com>
2021-12-11 23:12:35 +0200

    Resolve #6

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,28 @@
-export default function Home() {
-  return (
-    <h1>Hello world</h1>
-  )
+import PropTypes from 'prop-types'
+import Navigation from '../components/Base/Navigation.js'
+import Footer from '../components/Base/Footer.js'
+import BlankLayout from '../components/Layouts/BlankLayout.js'
+
+import cls from '../styles/home/main.module.css'
+
+
+const Home = () => {
+    return (
+        <BlankLayout>
+            <div className={cls.navigationWrapper}>
+                <Navigation />
+            </div>
+
+            <div>
+
+            </div>
+
+            <div className={cls.foorerWrapper}>
+                <Footer />
+            </div>
+        </BlankLayout>
+    )
 }
+
+
+export default Home 
