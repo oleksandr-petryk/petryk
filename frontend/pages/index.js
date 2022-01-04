4f6cdcfdf34aa390ad3965ea62c28688a27d83ed
Sasha-hk <sachapos50@gmail.com>
2022-01-04 22:00:58 +0200

    Resolve #36 again :)

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -12,6 +12,10 @@ const Home = () => {
     useEffect(() => {
         const vh = window.innerHeight * 0.01;
         const wrapper = document.querySelector('.wrapper')
+        vh = window.innerHeight * 0.01;
+        wrapper.style.minHeight = '0'
+        wrapper.style.height = vh * 100 + 'px'
+        
         window.addEventListener('resize', () => {
             vh = window.innerHeight * 0.01;
             wrapper.style.minHeight = '0'
