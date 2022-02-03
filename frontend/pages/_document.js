b40773b5e3e49381de40be2961cebb93de105936
Petryk Oleksandr <sachapos50@gmail.com>
2022-02-04 00:54:44 +0200

    Add Googgle Analytics script

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -9,6 +9,17 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='ture' />
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
+                    {/* Global site tag (gtag.js) - Google Analytics */}
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-21V0TCK3VD"></script>
+                    <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-21V0TCK3VD');
+                    </script>
+                    
                     <title>Petryk Oleksandr</title>
                 </Head>
                 <body>
