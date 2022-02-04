22151096d09b38bd7a72cbe50f222f41c10e66d9
Petryk Oleksandr <sachapos50@gmail.com>
2022-02-04 17:27:40 +0200

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 
-BASE_DIR=${PWD}
-echo $BASE_DIR
-
+# check exists .env file
 if [ -f ".env" ]; then
-    source .env
+    # export variables
+    while read line; do
+        export $line
+    done < .env
 
+    # create nginx directory if it is not exists
     if [ ! -d "nginx" ]; then
         . ./configure-nginx.sh
     fi
 
-    # Make symbol link for nginx config file
+    # make symbol link for nginx config file if not exists
     if [ ! -f "/etc/nginx/sites-enabled/${PROJECT_NAME}.conf" ]; then
-        ln -s ${BASE_DIR}/nginx/${PROJECT_NAME}.conf /etc/nginx/sites-enabled
+        ln -s ${PWD}/nginx/${PROJECT_NAME}.conf /etc/nginx/sites-enabled
     fi
-
+    
+    # build Next.js and run it
     docker-compose build
     docker-compose up -d
 else
