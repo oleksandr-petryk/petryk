4eb7e87d0a1704012a363169463c66f18d7b2e7d
Petryk Oleksandr <sachapos50@gmail.com>
2022-04-15 10:30:09 +0300

    feat: add Twitter lint

--- frontend/components/Base/Footer.js
+++ frontend/components/Base/Footer.js
@@ -1,6 +1,3 @@
-import PropTypes from 'prop-types'
-
-
 const Footer = () => {
     return (
         <footer>
@@ -8,10 +5,10 @@ const Footer = () => {
                 <a href="http://instagram.com/petryk_oleksandr" target="_blank">Instagram</a>
                 <a href="http://github.com/Sasha-hk" target="_blank">GitHub</a>
                 <a href="https://www.linkedin.com/in/oleksandr-petryk-905095175/" target="_blank">LinkedIn</a>
+                <a href="https://twitter.com/SashaPetryk" target="_blank">Twitter</a>
             </div>
         </footer>
     )
 }
-
 
 export default Footer
