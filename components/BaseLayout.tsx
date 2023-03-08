3ea72fcf3c9d09c19ce9adb55cb5677c092cb5e5
Oleksandr Petryk <sachapos50@gmail.com>
2023-03-08 15:44:59 +0200

    chore: start rewrite

--- components/BaseLayout.tsx
+++ components/BaseLayout.tsx
@@ -1,15 +1,9 @@
-import dynamic from 'next/dynamic';
 import type React from 'react';
-import { FC } from 'react';
+import type { FC } from 'react';
 
 import { NavBar } from 'components/NavBar';
 import { useRecoilValue } from 'recoil';
 import { layoutState } from 'state/layout.state';
-
-const BackgroundGradient = dynamic(
-  () => import('../components/BackgroundGradient'),
-  { ssr: false }
-);
 
 export const BaseLayout: FC<{
   children: React.ReactNode,
@@ -24,8 +18,6 @@ export const BaseLayout: FC<{
       <div className={['2xl:py-16 sm:pt-4 sm:pb-24', layout.fullFrame || fullFrame ? 'h-full' : 'min-h-full'].join(' ')}>
         {children}
       </div>
-
-      <BackgroundGradient />
     </>
   )
 }
