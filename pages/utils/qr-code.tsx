02089492bb81408e6045cec3036ccc285913d911
Oleksandr Petryk <sachapos50@gmail.com>
2023-03-16 21:18:01 +0200

    feat: add qr code generator utility

---
+++ pages/utils/qr-code.tsx
@@ -0,0 +1,48 @@
+import type { NextPage } from "next";
+import { useState } from "react";
+import QRCode from "qrcode.react";
+
+const QrCode: NextPage = () => {
+  const [inputValue, setValue] = useState("");
+
+  const downloadQR = () => {
+    const canvas = document.getElementById("qr-code-canvas")!;
+    const pngUrl = canvas
+      // @ts-ignore
+      .toDataURL("image/png")
+      .replace("image/png", "image/octet-stream");
+
+    let downloadLink = document.createElement("a");
+    downloadLink.href = pngUrl;
+    downloadLink.download = "123456.png";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
+  return (
+    <div className="container flex flex-col items-center">
+      <h1 className="py-4">QR code generator</h1>
+      <input
+        value={inputValue}
+        onChange={(e) => setValue(e.target.value)}
+        type="text"
+        placeholder="URL"
+        className="p-2 rounded mb-4"
+      />
+
+      <QRCode
+        id="qr-code-canvas"
+        value={inputValue}
+        size={290}
+        level={"H"}
+        includeMargin={true}
+      />
+      <a onClick={downloadQR} className="mt-4">
+        Download QR
+      </a>
+    </div>
+  );
+};
+
+export default QrCode;
